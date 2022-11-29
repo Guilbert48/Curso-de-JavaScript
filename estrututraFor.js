4a97d6bbe94507c4473516c242e1fe1e378ba124
Guil_NtC <guilberts48@gmail.com>
2022-11-29 08:24:34 -0300

    Estrutura for

---
+++ estrututraFor.js
@@ -0,0 +1,14 @@
+// Estrutura de repetição for
+
+for(var c = 0; c <= 10; c++ ){
+    console.log(`passo ${c}`)
+
+}
+
+//podemos percorrer arrays com for
+
+var arry = ['teste1','teste2','teste3','teste4','teste5','teste6','teste7','teste8']
+
+for(c = 0; c <= arry.length; c++){
+    console.log(`O valor do índice ${c} é ${arry[c]}0 `)
+}
